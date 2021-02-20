9de3a90c1623fc6a1d9792b59330c309d9866fc9
Mctom_Spdo <mctom.spdo@gmail.com>
2021-02-20 23:56:57 +0100

    beta design

--- files/js/settings.js
+++ files/js/settings.js
@@ -2,6 +2,6 @@ let whiteModeLink = `<link rel="stylesheet" href="./files/css/white-mode.css.css
 let darkModeLink = `<link rel="stylesheet" href="./files/css/dark-mode.css">`;
 let mode = "select";
 
-function getMode() {
-
+function MenuPress() {
+    
 }
