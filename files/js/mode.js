fdd14a7413ba9a47ce93ab5a9f67a08fa2b6b7b1
Mctom_Spdo <mctom.spdo@gmail.com>
2021-02-20 21:49:21 +0100

    First design Ideas

---
+++ files/js/mode.js
@@ -0,0 +1,7 @@
+let whiteModeLink = `<link rel="stylesheet" href="./files/css/white-mode.css.css">`;
+let darkModeLink = `<link rel="stylesheet" href="./files/css/dark-mode.css">`;
+let mode = "select";
+
+function getMode() {
+
+}
